c064dd4cd26e631790d0668666c3a0ef7029a8f1
ravikant <ravikant@CSZB01.chinasystems-me.com>
2018-12-03 06:07:39 +0400

    Initial Commit

---
+++ WebApplicationDemo/src/main/java/com/chanikya/learning/spring/WebApplicationDemo/WebApplicationDemoApplication.java
@@ -0,0 +1,12 @@
+package com.chanikya.learning.spring.WebApplicationDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplicationDemoApplication.class, args);
+	}
+}
